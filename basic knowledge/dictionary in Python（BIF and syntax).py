7546c9ce2db66568ac848a6519f5954679bbe626
kevin ma <mahaibin97@gmail.com>
2017-07-26 11:30:57 +0800

    dictionary
    字典

---
+++ basic knowledge/dictionary in Python（BIF and syntax).py
@@ -0,0 +1,3 @@
+Python字典，详见blog：
+	http://blog.csdn.net/liang19890820/article/details/72510715
+
--- basic knowledge/test-3.py
+++ basic knowledge/test-3.py
@@ -1,7 +1,16 @@
 
-
+'''
 infile=open("test-1.txt","r")
 
 for i in range(5):
       line=infile.readline()
       print(line[:-1])
+
+
+#字典中的遍历
+
+for key in dictionaryName:
+      print(key+":"+str(dictionaryName[]))
+
+
+ '''
