bb648cf47d1138749b586db0909603883ae9c9e5
kevin ma <mahaibin97@gmail.com>
2017-07-31 17:00:07 +0800

    引入正则表达式库re

---
+++ spider of Python/引入正则表达式re.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+r=requests.get("http://python123.io/ws/demo.html")
+demo=r.text
+
+soup=BeautifulSoup(demo,"html.parser")
+
+#find_all()方法中的五个参数---name
+for tag in soup.find_all(re.compile('b')):
+	print(tag.name)
+
+#find_all()方法中的五个参数---attrs
+for link in soup.find_all(id=re.compile('link')):
+	print(link)
+
+#find_all()方法中的五个参数---recursive
+
+
+#find_all()方法中的五个参数---string
+soup.find_all(string=re.compile("pyhton"))
