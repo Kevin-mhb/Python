c172ceedd935f7e4dd193d8d99e6a29aa3a2f733
kevin ma <mahaibin97@gmail.com>
2017-08-02 19:15:41 +0800

    优化版

--- spider of Python/中国最好大学排名(优化版）.py
+++ spider of Python/中国最好大学排名(优化版）.py
@@ -17,18 +17,18 @@ def fillUnivList(ulist, html):
     for tr in soup.find('tbody').children:
         if isinstance(tr, bs4.element.Tag):
             tds = tr('td')
-            ulist.append([tds[0].string, tds[1].string, tds[2].string,tds[3].string])
+            ulist.append([tds[0].string, tds[1].string, tds[2].string])
  
 def printUnivList(ulist, num):
     tplt = "{0:^10}\t{1:{3}^10}\t{2:^10}\t{3:^10}"
-    print(tplt.format("排名","学校名称","总分","指标得分",chr(12288)))
+    print(tplt.format("排名","学校名称","总分",chr(12288)))
     for i in range(num):
         u=ulist[i]
-        print(tplt.format(u[0],u[1],u[2],u[3],chr(12288)))
+        print(tplt.format(u[0],u[1],u[2],chr(12288)))
      
 def main():
     uinfo = []
-    url = 'http://www.zuihaodaxue.cn/zuihaodaxuepaiming2016.html'
+    url = 'http://www.zuihaodaxue.cn/zuihaodaxuepaiming2017.html'
     html = getHTMLText(url)
     fillUnivList(uinfo, html)
     printUnivList(uinfo, 20) # 20 univs
--- spider of Python/中国最好大学排名.py
+++ spider of Python/中国最好大学排名.py
@@ -18,11 +18,11 @@ def fillu(ulist,html):
       for tr in soup.find('tbody').children:          #tr 标签
             if isinstance(tr,bs4.element.Tag):
                   tds=tr('td')                        #td
-                  ulist.append([tds[0].string,tds[1].string,tds[2].string])
+                  ulist.append([tds[0].string,tds[1].string,tds[3].string])
 
 def printu(ulist,num):
       tplt="{0:^10}\t{1:{3}^10}\t{2:^10}"
-      print(tplt.format("排名","学校名称","总分"，chr(12288)))
+      print(tplt.format("排名","学校名称","总分"))
       for i in range(num):
             u=ulist[i]
             print(tplt.format(u[0],u[1],u[2],chr(12288)))
